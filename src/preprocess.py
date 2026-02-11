5d74779c2bc75d5bb6c9560753500e7810c74cea
hananmorsel <160420060+hananmorsel@users.noreply.github.com>
2026-02-11 11:47:44 +0200

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+def load_logs(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        logs = f.readlines()
+    logs = [line.strip() for line in logs]
+    return logs
+
+def create_dataframe(logs):
+    return pd.DataFrame({"log": logs})
 
